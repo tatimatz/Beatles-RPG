a92bb9f7cb5455194ce2b5a81bbf9e5a5f558c1f
Tatiana Matzenbacher <tatimatz@gmail.com>
2020-09-09 17:23:53 +0200

    Beatles RPG fst version

---
+++ app.js
@@ -0,0 +1,78 @@
+import { episodes } from "./episodes.js";
+
+console.log(episodes);
+// D.R.Y !!!
+const currentEpisodeElement = document.getElementById("current-episode");
+
+/**
+ *
+ * @param {String} episodeTitle
+ * @returns {Number} the given episode index in episodes array
+ */
+function findNextEpisodeIndex(episodeTitle) {
+  if (episodeTitle === "Get Back") {
+    return 0;
+  }
+  if (episodeTitle === "Go to a Sgt. Peppers Lonely Hearts Club Band concert") {
+    return 7;
+  }
+  if (episodeTitle === "Call Doctor Robert") {
+    return 8;
+  }
+  if (episodeTitle === "Call Nowhere Man") {
+    return 18;
+  }
+
+  for (let i = 0; i < episodes.length; i++) {
+    if (episodes[i].title === episodeTitle) return i;
+  }
+  return -1;
+}
+
+function prepareNextEpisode(evt) {
+  console.log(evt, evt.target, evt.target.textContent);
+  const nextTitle = evt.target.textContent;
+  const nextIndex = findNextEpisodeIndex(nextTitle);
+  if (nextIndex !== -1) render(episodes[nextIndex]);
+}
+
+function render(episode) {
+  console.log("current episode :", episode);
+  let template = `<div id="background">
+    <h1 class="episode-title">${episode.title}</h1>
+    <div class="main">
+    <img id="episode-image" src="./assets/img/${episode.image}" alt="image"/>
+    <div class="episodes-description des-strawberry">
+        <p>${episode.description}"</p>
+    <audio id="soundtrack" src="./assets/audio/${episode.audio}"></audio>
+    </div>
+    </div>
+    </div>`;
+
+  if (episode.choices) {
+    template += `<div class="buttons">
+        <button id="btn-1">${episode.choices[0]}</button>
+        <button id="btn-2">${episode.choices[1]}</button>
+    </div>
+    `;
+  }
+
+  currentEpisodeElement.innerHTML = template;
+
+  const audio = currentEpisodeElement.querySelector("#soundtrack");
+  audio.play();
+
+  const button1 = document.getElementById("btn-1");
+  button1.addEventListener("click", prepareNextEpisode);
+
+  const button2 = document.getElementById("btn-2");
+  button2.addEventListener("click", prepareNextEpisode);
+
+  if (episode.choices.length === 1) {
+    const noButton = document.querySelector(".buttons");
+    var element = document.getElementById("btn-2");
+    noButton.removeChild(element);
+  }
+}
+
+render(episodes[0]);
---
+++ episodes.js
@@ -0,0 +1,145 @@
+export const episodes = [
+  {
+    title: "Strawberry Fields Forever",
+      description:
+      "So Billy Shears is at Strawberry Fields. Depressed and lost, all that he wants to do is going back home, his lovely Yellow Submarine and meet his fellows. So he decides to:",
+      image: "Billy-Shears-lost.png",
+      audio: "Strawberry.mp3",
+      choices: ["I'll Follow the Sun", "Let It Be"],
+    },
+  {
+    title: "I'll Follow the Sun",
+    description:
+      "But tomorrow may rain so I'll follow the sun. Billy Shears feels a little bit better now but still... lost. Suddenly, something in the way she moves… attracts him like no other lover. Our hero falls in love! And guess by who?",
+    image: "Follow-the-sun.jpg",
+    audio: "Follow.mp3",
+    choices: ["Michelle", "Eleanor Rigby"],
+  },
+  {
+    title: "Let It Be",
+    description:
+      "He stays still like a fool on the hill, just watching the landscape when someone whispers in his ear:",
+    image: "Let-it-be.jpg",
+    audio: "Let.mp3",
+    choices: ["Blackbird", "Maggie Mae"],
+  },
+  {
+    title: "Michelle",
+    description:
+      "Michelle ma belle, sont les mots qui vont très bien ensemble… Our buddy has a coup de foudre, and the romantic young couple spends beautiful moments together. Billy feels like in heaven! But his french pretty girl are… moving back to Paris! And Billy needs to find his way back home as well. So he starts his lonely adventure bounding:",
+    image: "Michelle.jpg",
+    audio: "Michelle.mp3",
+    choices: ["Across the Universe", "Here, There and Eveywhere"],
+  },
+  {
+    title: "Eleanor Rigby",
+    description:
+    "What a trap… Eleanor Rigby leaves in a dream and is a lonely person. Billy is getting more depressed! What should he do?",
+    image: "Eleanor-Rigby.png",
+    audio: "Eleanor.mp3",
+    choices: ["Go to a Sgt. Peppers Lonely Hearts Club Band concert", "Call Doctor Robert"],
+  },
+  {
+    title: "Across the Universe",
+    description:
+    "So Jah, Guru, Devah open our heros’ mind, he frees his spirit and he’s ready to find:",
+    image: "Across.jpg",
+    audio: "Across.mp3",
+    choices: ["Sexy Sadie", "Nowhere Man"],
+  },
+  {
+    title: "Here, There and Eveywhere",
+    description:
+    "But Billy doesn’t forget his love. He’s suffering and the only way to relief his heart is going back home and give a hug to his fellows. Suddenly, he finds a wallet. Hum, weird… he opens it and…",
+    image: "Here.jpg",
+    audio: "Here.mp3",
+    choices: ["Baby, you're a Rich Man", "Cry Baby, Cry"],
+  },
+  {
+    title: "Sgt. Peppers Lonely Hearts Club Band",
+    description:
+    "Oh, what a moment! Billy sings, dances, drinks, meets people… Suddenly he finds a wallet. Hum, weird… he opens it and…",
+    image: "Sgt-Peppers.jpg",
+    audio: "Sgt.mp3",
+    choices: ["Baby, you're a Rich Man", "Cry Baby, Cry"],
+  },
+  {
+    title: "Doctor Robert",
+    description:
+    "Well well well, I’m feeling fine… Our hero feels more… relaxed. So he goes on the street and he takes:",
+    image: "Dr-Robert.jpg",
+    audio: "Doctor.mp3",
+    choices: ["Penny Lane", "The Long and Winding Road"],
+  },
+  {
+    title: "Baby, you're a Rich Man",
+    description:
+    "Great!! Now he can pay someone to help him. But who?",
+    image: "Rich-Man.jpg",
+    audio: "Rich.mp3",
+    choices: ["Lucy in the Sky with Diamonds", "Nowhere Man"],
+  },
+  {
+    title: "The Long and Winding Road",
+    description:
+    "So this long and winding road leads to a door. Billy has seen this road before… Finally he knocks the door. Who opens it?",
+    image: "Road.jpg",
+    audio: "Long.mp3",
+    choices: ["Lucy in the Sky with Diamonds", "Nowhere Man"],
+  },
+  {
+    title: "Blackbird",
+    description:
+    "And the Blackbird says to him: « take these broken wings and learn to fly! » Billy Shears has an insight and decides to go:",
+    image: "Blackbird.jpeg",
+    audio: "Blackbird.mp3",
+    choices: ["Across the Universe", "Here, There and Eveywhere"],
+  },
+  {
+  title: "Maggie Mae",
+    description:
+    "Bad influence, this dirty Maggie Mae. She ran away from the judge and now offers to Billy a wallet. Hum… He opens the wallet anyway and:",
+    image: "Maggie.jpg",
+    audio: "Maggie.mp3",
+    choices: ["Baby, you're a Rich Man", "Cry Baby, Cry"],
+  },
+  {
+    title: "Cry Baby, Cry",
+    description:
+    "Oh no!! Billy has spent all the money but the wallet belongs to… Rocky Raccoon! The cowboy gets mad and he dares our hero to a duel. So what does he do??",
+    image: "Cry.jpg",
+    audio: "Cry.mp3",
+    choices: ["Call Nowhere Man", "Get Back"],
+  },
+  {
+    title: "Sexy Sadie",
+    description:
+    "Noooooo bad choice! He made a fool of everyone, run away Billy! Go find Nowhere Man, he’s a better company for you!",
+    image: "Sexy-Sadie.jpg",
+    audio: "Sexy.mp3",
+    choices: ["Nowhere Man"],
+  },
+  {
+    title: "Lucy in the Sky with Diamonds",
+    description:
+    "So our buddy follows the girl with kaleidoscope eyes down to a bridge by a fountain, on a train in a station… and then she stops and says: 'Nowhere man has the answer to your questions'.",
+    image: "Lucy.jpeg",
+    audio: "Lucy.mp3",
+    choices: ["Nowhere Man"],
+  },
+  {
+    title: "Penny Lane",
+    description:
+    "So he decides to enter that barber shop showing photographs and guess who he finds?",
+    image: "Penny-Lane.jpg",
+    audio: "Penny.mp3",
+    choices: ["Nowhere Man"],
+  },
+  {
+    title: "Nowhere Man",
+    description:
+    "Gotcha!! He’s a real nowhere man sitting in his nowhere land, but he’s the capitan’s best buddy! Nowhere man leads Billy Shears way back to the Yellow Submarine!!",
+    image: "Nowhere-man.png",
+    audio: "Nowhere.mp3",
+  },
+];
